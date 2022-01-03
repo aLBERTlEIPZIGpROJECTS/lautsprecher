bd971ac8272ea999d0658926a2a7a2c36e3ceea7
AlbertLeipzig <albertclemvill@gmail.com>
2022-01-03 13:22:01 +0100

    articles components. where's my scss?

--- src/components/ArticleEntryCard.js
+++ src/components/ArticleEntryCard.js
@@ -1,9 +1,9 @@
 import ArrowRight from "../materials/icons/arrow-right.png"
 
-const ArticleEntryCard = () => {
+const ArticleEntryCard = (props) => {
     return(
         <div className="article-entry-card">
-            <p className="article__title">Title</p>
+            <p className="article__title">{props.title}</p>
             <img src={ ArrowRight } alt="" className="icon" />
         </div>
     )
--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,16 +1,39 @@
 import ArticleCard from "../components/ArticleEntryCard"
+import { useState, useEffect } from "react"
 
+/* fetch("http://localhost:4000/musicians")
+    .then(res => res.json())
+    .then(json => 
+        json.map(data => {
+            <ArticleCard title = {data.bandName}/>
+        })) */
+        
+        
 const Articles = () => {
+    
+    useEffect(() => {
+        fetch("http://localhost:4000/musicians")
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                console.log(data)
+            })
+    
+    }, [])
+
     return(
         <div className="articles_container">
             <h1>Alle Artikel</h1>
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
-            <ArticleCard />
+
+          
+           <ArticleCard title = {"hello"} />
+            <ArticleCard title = {"bye"} />
+            <ArticleCard title = {"today"} />
+            <ArticleCard title = {"hello"} />
+            <ArticleCard title = {"hello"} />
+            <ArticleCard title = {"hello"} />
+            <ArticleCard title = {"hello"} />
         </div>
     )
 }

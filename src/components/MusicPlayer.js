0794d68bc95213bd0b9a9d880d9b6e670dc44aa8
AlbertLeipzig <albertclemvill@gmail.com>
2022-01-17 13:43:55 +0100

    music player not working yet

--- src/components/MusicPlayer.js
+++ src/components/MusicPlayer.js
@@ -1,20 +1,25 @@
 import { useState } from "react"
-import musicplayer from "../materials/icons/trumpet.gif"
+import playerIcon from "../materials/icons/trumpet.gif"
 import track from "../materials/tracks/Studio 2020 - Quincas Moreira.mp3"
 
 const MusicPlayer = () => {
 
-    const [ playerClass, setPlayerClass ] = useState("stop")
+    const [ playerClass, setPlayerClass ] = useState("icon-style__off")
+    const [ playerStatus, setPlayerStatus ] = useState("mute")
+
 
     const playMusic = () => {
-        setPlayerClass("play")
+        playerClass === "icon-style__off" ? setPlayerClass("icon-style__on") : setPlayerClass("icon-style__off")
+
+        playerStatus === "mute" ? setPlayerStatus("") : setPlayerStatus("mute")
     }
 
     
     return(
-        <div className="musicPlayer">
+        <div className="music-player">
+            <img src={ playerIcon } alt="" onClick={ playMusic } className= { playerClass }/>
             <audio
-                controls autoPlay
+                controls /* autoPlay   */
                 src= {track }  className="player">
             </audio>
         </div>

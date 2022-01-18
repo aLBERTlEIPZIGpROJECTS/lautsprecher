743d3f9932517b8383ae9de06beb5faf7f46f66c
AlbertLeipzig <albertclemvill@gmail.com>
2022-01-18 11:18:21 +0100

    search bar only for musicians

--- src/components/MusiciansTab.js
+++ src/components/MusiciansTab.js
@@ -4,14 +4,22 @@ import useFetch from "../useFetch"
 
 const MusiciansTab = ( props ) => {
 
-  const { data, isLoading, error } = useFetch("http://localhost:8000/musicians")
+    const { data, isLoading, error } = useFetch("http://localhost:8000/musicians")
+    const [ searchTerm, setSearchTerm ] = useState("")
 
     return(
         <div className= {`${ props.class}`} >
             { error && <div>{ error }</div>}
             { isLoading && <div>Loading...</div>}
+            <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
             {
-              data &&  data.map((musician) => (
+              data &&  data.filter((val) => {
+                  if(searchTerm === ""){
+                      return val
+                  } else if(val.fname.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())){
+                      return val
+                  }
+              }).map((musician) => (
                     <MusicianCard fname = {musician.fname} lname = { musician.lname} instrument = { musician.instrument } style = { musician.style} mail = { musician.mail } phone = { musician.phone } bands = { musician.bands } fb = { musician.fb } tw = { musician.tw} ig = { musician.ig} sc = { musician.sc }  key = { musician.id } img = { musician.img }/>
                 ))
             }

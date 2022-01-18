1ac539ac4cef3fcfbafcde127fb44cf79ebbec8c
AlbertLeipzig <albertclemvill@gmail.com>
2022-01-18 12:07:43 +0100

    multiple search fields?

--- src/components/MusiciansTab.js
+++ src/components/MusiciansTab.js
@@ -6,12 +6,21 @@ const MusiciansTab = ( props ) => {
 
     const { data, isLoading, error } = useFetch("http://localhost:8000/musicians")
     const [ searchTerm, setSearchTerm ] = useState("")
+    /* const [ searchDate, setSearchDate ] = useState(new Date()) */
+
+    /*
+        a) does onChange applies to date-input?
+        b) wrap multiple states in a single onChange event
+        c) change both states with a single function
+    */
+
 
     return(
         <div className= {`${ props.class}`} >
             { error && <div>{ error }</div>}
             { isLoading && <div>Loading...</div>}
             <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+            {/* <input type="date" onChange={setSearchDate} /> */}
             {
               data &&  data.filter((val) => {
                   if(searchTerm === ""){
